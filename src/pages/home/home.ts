3ad262df0cf750af2e27004399a87a08d7d4348e
moomam6934 <destinationpainbm@gmail.com>
2017-09-22 16:55:10 +0700

    GetData_QRCode

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  text;
+  format;
+  showmoney;
+  money;
+  phonenumber;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public scanner: BarcodeScanner) {
+
+  }
+  scan() {
+    this.scanner.scan()
+      .then(
+      (result) => {
+        this.text = result.text;
+        this.format = result.format;
+        // money
+        this.showmoney = result.text.substr(63);
+        this.money = this.showmoney.slice(0, -15) + 'บาท';
+        // phone
+        this.phonenumber = '+' + result.text.substr(42, 11);
+      }
+      , (error) => { }
+      );
 
   }
 
